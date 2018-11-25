35ebca0b95cc8e53dce8ac3dbb0e301179871715
Ilechukwu Alex <ilechukwu_alex2001@yahoo.com>
2018-11-25 10:20:24 +0100

    first commit

---
+++ Wifi Home Automation/main.c
@@ -0,0 +1,18 @@
+/*
+ * Wifi Home Automation.c
+ *
+ * Created: 11/25/2018 10:06:19 AM
+ * Author : ALEX
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
